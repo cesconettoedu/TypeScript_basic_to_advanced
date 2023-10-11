d56095818ecd89aac336a11f682c0c93ad26fc27
Eduardo <cesconettoedu@gmail.com>
2023-10-11 12:58:05 -0400

    start

--- 1_INTRO/index.js
+++ 1_INTRO/index.js
@@ -1,9 +1,15 @@
-var firstName = "Eduardo";
-var anotherName = 1;
-var x = true;
-function greeting(name) {
-    console.log("Ola " + name);
-}
-greeting(firstName);
+// const firstName = "Eduardo"
+// const anotherName = 1
+// const x = true
+// function greeting(name: string) {
+//   console.log("Ola " + name)
+// }
+// greeting(firstName)
 // greeting(anotherName)
 // greeting(x)
+var numA = 43;
+var numB = 4;
+function somaNumber(a, b) {
+    console.log(a + b);
+}
+somaNumber(numA, numB);
--- 1_INTRO/index.ts
+++ 1_INTRO/index.ts
@@ -1,11 +1,21 @@
-const firstName = "Eduardo"
-const anotherName = 1
-const x = true
+// const firstName = "Eduardo"
+// const anotherName = 1
+// const x = true
 
-function greeting(name: string) {
-  console.log("Ola " + name)
-}
+// function greeting(name: string) {
+//   console.log("Ola " + name)
+// }
 
-greeting(firstName)
+// greeting(firstName)
 // greeting(anotherName)
 // greeting(x)
+
+const numA = 43
+const numB = 4
+
+function somaNumber (a: number, b: number){
+  console.log(a + b);
+  
+}
+
+somaNumber(numA, numB);
