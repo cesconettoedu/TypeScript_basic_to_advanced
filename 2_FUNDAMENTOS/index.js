077f66064baeda913bc21cbc4186b2c31edf6175
Eduardo <cesconettoedu@gmail.com>
2023-10-12 10:58:52 -0400

    fundamentos

--- 2_FUNDAMENTOS/index.js
+++ 2_FUNDAMENTOS/index.js
@@ -1,26 +1,34 @@
+"use strict";
 // 1 - numbers
-var x = 10;
+let x = 10;
 console.log(x);
 x = 16;
 console.log(typeof x);
-var y = 15.123465;
+const y = 15.123465;
 console.log(y.toPrecision(4)); // utiliza metodos de numeros somente
 // 2 - string
-var firstName = "Eduardo";
+const firstName = "Eduardo";
 console.log(firstName.toUpperCase()); // utiliza metodos de string somente
-var fullName;
-var lastName = "Cesconetto";
+let fullName;
+const lastName = "Cesconetto";
 fullName = firstName + " " + lastName;
 console.log(fullName);
 console.log(typeof fullName);
 // 3 - boolean
-var a = false;
+let a = false;
 console.log(a);
 console.log(typeof a);
 a = true;
 console.log(a);
 // 4 - inference e annotation
-var ann = "Teste";
-var inf = "Teste";
+let ann = "Teste";
+let inf = "Teste";
 // ann = 1
 // inf = 1
+// tsc -w      no terminal ele fica compilando automatico assim que salvar
+console.log("testando");
+//exercicio  -mudar variavel numero par string
+let nX = 1980;
+let numString = nX.toString();
+const printNumber = `Vai imprimir o ${numString}`;
+console.log(printNumber);
--- 2_FUNDAMENTOS/index.ts
+++ 2_FUNDAMENTOS/index.ts
@@ -45,4 +45,17 @@ let inf = "Teste"
     // ann = 1
     // inf = 1
 
+// tsc -w      no terminal ele fica compilando automatico assim que salvar
+
+console.log("testando");
+
+//exercicio  -mudar variavel numero par string
+
+let nX: number = 1980
+
+let numString = nX.toString()
+
+const printNumber: string = `Vai imprimir o ${numString}`
+
+console.log(printNumber);
 
