360fca225b5490ef6ffbf73c94afc2b9fb1c4114
Eduardo <cesconettoedu@gmail.com>
2023-10-17 09:15:49 -0400

    comeco 5 funcoes

---
+++ 5_FUNCOES/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// 1 -void
+//funcao que nao tem clausula de retorno
+function withoutReturn() {
+    console.log("esta funcao nao tem retorno");
+}
+withoutReturn();
+console.log("----------2----------");
---
+++ 5_FUNCOES/index.ts
@@ -0,0 +1,9 @@
+// 1 -void
+    //funcao que nao tem clausula de retorno
+
+    function withoutReturn():void {
+        console.log("esta funcao nao tem retorno");
+    }
+
+    withoutReturn()
+console.log("----------2----------");
