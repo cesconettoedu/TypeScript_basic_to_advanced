ba4b87ccac4ba2c92c2310106787408d8c185247
Eduardo <cesconettoedu@gmail.com>
2023-10-17 10:49:15 -0400

    done section functions

--- 5_FUNCOES/index.js
+++ 5_FUNCOES/index.js
@@ -60,3 +60,53 @@ function mergeArrays(arr1, arr2) {
 console.log(mergeArrays([1, 2, 3], [5, 6]));
 //console.log(mergeArrays([1, 2, 3], ["teste", "testando"]))    para unir tem q tipar igual abaixo
 console.log(mergeArrays([1, 2, 3], ["teste", "testando"]));
+console.log("----------6----------");
+// 6 - Parametros opcionais
+function modernGreeting(name, greet) {
+    if (greet) {
+        return `Ola ${greet} ${name}, tudo bem ?`;
+    }
+    return `Ola ${name}, tudo bem ?`;
+}
+console.log(modernGreeting("Eduardo"));
+console.log(modernGreeting("Lucca", "Dr."));
+console.log("----------7----------");
+// 7 - parametro default
+function somaDefault(n, m = 10) {
+    return n + m;
+}
+console.log(somaDefault(10));
+console.log(somaDefault(15, 12)); // o m esta com defaul 10, mas se colocar outro ele troca
+console.log("----------8----------");
+// 8 - tipo unknown
+function doSomething(x) {
+    if (Array.isArray(x)) {
+        console.log(x[0]);
+    }
+    else if (typeof x === "number") {
+        console.log("X e um numero");
+    }
+}
+doSomething([1, 2, 3]);
+doSomething(5);
+console.log("----------9----------");
+// 9 - tipo Never
+function showErrorMessage(msg) {
+    throw new Error(msg);
+}
+//showErrorMessage("Algum error")
+console.log("----------10----------");
+// 10 - Rest operator
+function sumAll(...n) {
+    return n.reduce((number, sum) => sum + number);
+}
+console.log(sumAll(1, 2, 3, 4, 5));
+console.log(sumAll(5, 324, 65645));
+//console.log(sumAll("teste"))    teste nao 'e numerico
+console.log("----------11----------");
+// 11 - Destructuring como paramentro
+function showProductDetails({ name, price }) {
+    return `O nome do produto 'e ${name} e ele custa R$${price}`;
+}
+const shirt = { name: "Camisa", price: 49.98 };
+console.log(showProductDetails(shirt));
--- 5_FUNCOES/index.ts
+++ 5_FUNCOES/index.ts
@@ -88,3 +88,83 @@ console.log("----------5----------");
     //console.log(mergeArrays([1, 2, 3], ["teste", "testando"]))    para unir tem q tipar igual abaixo
       console.log(mergeArrays<number | string>([1, 2, 3], ["teste", "testando"]))
     
+
+
+
+console.log("----------6----------");
+// 6 - Parametros opcionais
+    function modernGreeting(name: string, greet?: string) {
+
+        if(greet) {
+            return `Ola ${greet} ${name}, tudo bem ?`
+        }
+
+        return `Ola ${name}, tudo bem ?`
+    }
+
+    console.log(modernGreeting("Eduardo"));
+    console.log(modernGreeting("Lucca", "Dr."));
+    
+
+    
+
+
+console.log("----------7----------");
+// 7 - parametro default
+    function somaDefault(n: number, m = 10) {
+        return n + m
+    }
+
+    console.log(somaDefault(10));
+    console.log(somaDefault(15, 12));  // o m esta com defaul 10, mas se colocar outro ele troca
+ 
+    
+
+console.log("----------8----------");
+// 8 - tipo unknown
+    function doSomething(x: unknown) {
+        if(Array.isArray(x)) {
+            console.log(x[0]);
+        } else if(typeof x === "number") {
+            console.log("X e um numero");            
+        }
+    }
+
+    doSomething([1, 2, 3])
+    doSomething(5)
+
+
+
+console.log("----------9----------");
+// 9 - tipo Never
+    function showErrorMessage(msg: string): never {
+        throw new Error(msg)
+    }
+
+    //showErrorMessage("Algum error")
+
+
+
+
+console.log("----------10----------");
+// 10 - Rest operator
+    function sumAll(...n: number[]) {
+        return n.reduce((number, sum) => sum + number)
+    }
+
+    console.log(sumAll(1, 2, 3, 4, 5))  
+    console.log(sumAll(5, 324, 65645))   
+    //console.log(sumAll("teste"))    teste nao 'e numerico
+    
+    
+ 
+    
+console.log("----------11----------");
+// 11 - Destructuring como paramentro
+    function showProductDetails({name, price}: {name: string, price: number}):string {
+        return `O nome do produto 'e ${name} e ele custa R$${price}`
+    }
+
+    const shirt = {name: "Camisa", price: 49.98}
+
+    console.log(showProductDetails(shirt));
