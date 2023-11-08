9f64d21f54903cb99176c28d6b6b2df234980bb2
Eduardo <cesconettoedu@gmail.com>
2023-11-08 15:06:12 -0500

    fim da sessao 8

--- 8_CLASSES/index.js
+++ 8_CLASSES/index.js
@@ -156,4 +156,133 @@ class Nova extends Base {
 const myObject = new Nova();
 myObject.someMethod();
 console.log("------------11--------------");
-// 11 - 
+// 11 - Public
+class C {
+    constructor() {
+        this.x = 10; //public 'e o modo default nao muda nada
+    }
+}
+const cInstace = new C();
+console.log(cInstace.x);
+console.log("------------12--------------");
+// 12 - Protected
+class E {
+    constructor() {
+        this.x = 10;
+    }
+}
+class F extends E {
+    showX() {
+        console.log("X: " + this.x);
+    }
+}
+const fInstance = new F();
+fInstance.showX();
+console.log("------------13--------------");
+// 13 - Private
+class PrivateClass {
+    constructor() {
+        this.name = "Private";
+    }
+    showName() {
+        return this.name;
+    }
+    privateMethod() {
+        console.log("Metodo Privado");
+    }
+}
+const pObj = new PrivateClass();
+console.log(pObj.showName()); //so pode ser acessado pelo metodo
+// exemplo: tentando acessar o metodo privado.
+// class TestingPrivate extends PrivateClass {
+//     myMethod() {
+//       this.privateMethod()
+//     }
+// }
+console.log("------------14--------------");
+// 14 - static members
+class StaticMembers {
+    static straticMethod() {
+        console.log("Este e um metodo estatico");
+    }
+}
+StaticMembers.prop = "Teste Static"; //coloca o static para ter acesso a ela
+console.log(StaticMembers.prop);
+StaticMembers.straticMethod();
+console.log("------------15--------------");
+// 15 - generic class
+class Item {
+    constructor(first, second) {
+        this.first = first;
+        this.second = second;
+    }
+    get showFirst() {
+        return `O first e: ${this.first}`;
+    }
+}
+const newItem = new Item("caixa", "surpresa");
+console.log(newItem);
+console.log(newItem.showFirst);
+console.log(typeof newItem.first);
+const secondItem = new Item(12, true);
+console.log(secondItem);
+console.log(secondItem.showFirst);
+console.log(typeof secondItem.first);
+console.log("------------16--------------");
+// 16 - parameter properties
+//resumindo a sintax colocando no constructor
+class ParameterProperties {
+    constructor(name, qty, price) {
+        this.name = name;
+        this.qty = qty;
+        this.price = price;
+        this.name = name;
+        this.qty = qty;
+        this.price = price;
+    }
+    get showQty() {
+        return `Qtd total: ${this.qty}`;
+    }
+    get showPrice() {
+        return `Qtd total: ${this.price}`;
+    }
+}
+const newShirt = new ParameterProperties("Camisa", 5, 19.99);
+console.log(newShirt.name);
+//console.log(newShirt.price);  so pode ser acessa via metodo
+console.log(newShirt.showQty);
+console.log(newShirt.showPrice);
+console.log("------------17--------------");
+// 17 - class expressions
+const myClass = class {
+    constructor(name) {
+        this.name = name;
+    }
+};
+const pessoa = new myClass("Jones");
+console.log(pessoa);
+console.log(pessoa.name);
+console.log("------------18--------------");
+// 18 - abstract class
+class AbstractClass {
+}
+//const newObj = new AbstractClass();     nao pode criar uma instancia de uma class abstrata
+class AbstractExample extends AbstractClass {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+    showName() {
+        console.log(`O nome e: ${this.name}`);
+    }
+}
+const newAbstractObject = new AbstractExample("Josias");
+newAbstractObject.showName();
+console.log("------------19--------------");
+// 19 - relacoes entre classes
+class Dog {
+}
+class Cat {
+}
+const doguinho = new Cat();
+console.log(doguinho);
